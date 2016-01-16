b845f90060c2294f377834abe02dbba69edb66b7
Manu Phatak <bionikspoon@gmail.com>
2016-01-15 21:28:48 -0600

    rename tests, cleanup metaclass, passing tests on python2

--- tests/test_encode_multi.py
+++ tests/test_encode_multi.py
@@ -7,19 +7,23 @@ from pytest import mark
 
 import pureyaml
 from pureyaml.encoder import node_encoder
-from pureyaml.nodes import *
+from pureyaml.nodes import *  # noqa
 from tests.utils import MultiTestCaseBase
 
 
 class EncodeTestCase(MultiTestCaseBase):
     it_handles_dict__data = {'name': 'John Smith', 'age': 33}
-    it_handles_dict__test_dump__not_PYPY = dedent("""
+    it_handles_dict__test_pureyaml_dump__not_PYPY = dedent("""
         age: 33
         name: John Smith
     """)[1:]
-    it_handles_dict__test_dump__PYPY = dedent("""
+    it_handles_dict__test_pureyaml_dump__PYPY = dedent("""
         name: John Smith
         age: 33
+    """)[1:]
+    it_handles_dict__test_pyyaml_dump = dedent("""
+        age: 33
+        name: John Smith
     """)[1:]
     it_handles_dict__test_encode = Map(  # :off
         (Str('name'), Str('John Smith')),
@@ -27,7 +31,7 @@ class EncodeTestCase(MultiTestCaseBase):
     )  # :on
 
 
-def dump(data):
+def pureyaml_dump(data):
     text = pureyaml.dump(data)
     # print('\n' + text)
     return text
@@ -56,19 +60,19 @@ def test_encode(case):
     assert encode(data) == expected
 
 
-@mark.parametrize('case', EncodeTestCase.keys('dump'))
-def test_dump(case):
-    data, expected = EncodeTestCase.get('dump', case)
-    assert dump(data) == expected
+@mark.parametrize('case', EncodeTestCase.keys('pureyaml_dump'))
+def test_pureyaml_dump(case):
+    data, expected = EncodeTestCase.get('pureyaml_dump', case)
+    assert pureyaml_dump(data) == expected
 
 
-@mark.parametrize('case', EncodeTestCase.keys('dump'))
-def test_dump_pyyaml(case):
-    data, expected = EncodeTestCase.get('dump', case)
-    assert pyyaml_dump(data) == expected
-
-
-@mark.parametrize('case', EncodeTestCase.keys('dump'))
-def test_sanity(case):
-    data, _ = EncodeTestCase.get('dump', case)
+@mark.parametrize('case', EncodeTestCase.keys('pureyaml_dump'))
+def test_pureyaml_sanity(case):
+    data, _ = EncodeTestCase.get('pureyaml_dump', case)
     assert sanity(data) == data
+
+
+@mark.parametrize('case', EncodeTestCase.keys('pyyaml_dump'))
+def test_pyyaml_dump(case):
+    data, expected = EncodeTestCase.get('pyyaml_dump', case)
+    assert pyyaml_dump(data) == expected
--- tests/utils/multi_test_case.py
+++ tests/utils/multi_test_case.py
@@ -3,7 +3,8 @@
 from __future__ import absolute_import
 
 from collections import namedtuple
-from difflib import get_close_matches
+# from difflib import get_close_matches
+from warnings import warn
 
 from tests.utils import PY34, PY35, PY33
 
@@ -12,7 +13,7 @@ try:
 except ImportError:
     from future.moves.collections import OrderedDict, defaultdict
 
-from future.utils import with_metaclass, iteritems, PY26, PY27, PYPY
+from future.utils import with_metaclass, iteritems, PY26, PY27, PYPY, PY2, PY3, iterkeys
 
 
 def head_and_tail(x, *xs):
@@ -52,12 +53,17 @@ map_version = {  # :off
     'PY34': PY34,
     'PY35': PY35,
     'PYPY': PYPY,
+    'PY2': PY2,
+    'PY3': PY3,
     'not_PY26': not PY26,
     'not_PY27': not PY27,
     'not_PY33': not PY33,
     'not_PY34': not PY34,
     'not_PY35': not PY35,
     'not_PYPY': not PYPY,
+    'not_PY2': not PY2,
+    'not_PY3': not PY3,
+
 }  # :on
 
 
@@ -69,14 +75,34 @@ def is_correct_python_version(version_tags):
     return all(map(lambda tag: map_version[tag], version_tags))
 
 
+def prepare_py2(cls_dict):
+    # Doesn't keep test order, but prioritizes '__data' lines for easy error handling.
+    _cls_dict = OrderedDict()
+    for key, value in iteritems(cls_dict):
+        if str(key).endswith('__data'):
+            _cls_dict[key] = value
+
+    _cls_dict.update(cls_dict)
+    return _cls_dict
+
+
+# noinspection PyMethodParameters
 class MultiTestMeta(type):
+    # noinspection PyMethodOverriding
+    @classmethod
+    def __prepare__(cls, name, bases):
+        return OrderedDict()
+
     def __new__(cls, cls_name, bases, cls_dict):  # noqa
+        if PY26 or PY27:
+            cls_dict = prepare_py2(cls_dict)
+
         d = defaultdict(dict)
         d.update(dict(cls_dict))
         _data = namedtuple(cls_name, ['data', 'expected'])
         order = defaultdict(list)
 
-        test_q = {}
+        test_data_q = {}
 
         for name, value in iteritems(cls_dict):
             # Guard, skip internal object attributes
@@ -87,14 +113,18 @@ class MultiTestMeta(type):
 
             # Guard, store test data, and move on.
             if 'data' in meta:
-                test_q[test_name] = value
+                test_data_q[test_name] = value
                 continue
 
             # Guard, expectations defined before data (simplicity)
-            test_data = test_q.get(test_name, None)
+            test_data = test_data_q.get(test_name, None)
             if test_data is None:
                 # TODO detailed error message
-                raise AttributeError('missing test data')
+                warn(  # :off
+                    '{name} is improperly defined. Missing data definition: {test_name}__data'.format(**vars()),
+                    category=UserWarning
+                )  # :on
+                continue
 
             # Guard, skip specs for other versions of python
             version_meta_tags, meta = separate_version_tags(meta)
@@ -105,74 +135,82 @@ class MultiTestMeta(type):
             action_meta_tags, meta = separate_action_tags(meta)
             if not action_meta_tags:
                 # TODO detailed error message
-                raise AttributeError('missing meta tags')
+                warn(  # :off
+                    '{name} is improperly defined. Missing action tags'.format(**vars())                ,
+                    category=UserWarning
+
+                )  # :on
+                continue
 
             # add test to class
             for action in action_meta_tags:
-
                 d[action][test_name] = _data(test_data, value)
                 order[action].append(test_name)
 
-            # # setup
-            # is_data = name.endswith('__data')
-            # is_expected = name.endswith('__expected')
-            #
-            # # Guard, deal with the easy case.
-            # is_simple_definition = not is_data and not is_expected
-            # if is_simple_definition:
-            #     if len(value) != 2:
-            #         msg = '%r in class %r improperly configured.\n!! Expecting a 2-tuple, got: %r'
-            #         raise AttributeError(msg % (name, cls_name, value))
-            #     # Add onto ordered list.
-            #     d[name] = _data(*value)
-            #     order.append(name)
-            #     continue
-            #
-            # _name = name
-            # # rename attribute: strip suffix
-            # if is_data:
-            #     name = name[:-len('__data')]
-            # if is_expected:
-            #     name = name[:-len('__expected')]
-            #
-            # # remove original attributes from dict
-            # data = d.pop(name + '__data', None)
-            # expected = d.pop(name + '__expected', None)
-            #
-            # # Guard, improperly defined attribute or second visit
-            # if not data or not expected:
-            #     # second visit
-            #     if name in order:
-            #         continue
-            #
-            #     # error handling
-            #     if data:
-            #         missing_attr = name + '__expected'
-            #     elif expected:
-            #         missing_attr = name + '__data'
-            #     else:
-            #         missing_attr = 'Unknown'
-            #
-            #     choices = [key for key in cls_dict if key != _name]
-            #     close_match = get_close_matches(missing_attr, choices, 1)
-            #     if close_match:
-            #         misspelled = '\n\nDid you mistype  %r  ?' % close_match[0]
-            #     else:
-            #         misspelled = ''
-            #
-            #     msg = '%r in class %r improperly configured.\n!! Missing attribute: %r%s'
-            #     raise AttributeError(msg % (name, cls_name, missing_attr, misspelled))
-            #
-            # # Add onto ordered list.
-            # d[name] = _data(data, expected)
-            # order.append(name)
+                # # setup
+                # is_data = name.endswith('__data')
+                # is_expected = name.endswith('__expected')
+                #
+                # # Guard, deal with the easy case.
+                # is_simple_definition = not is_data and not is_expected
+                # if is_simple_definition:
+                #     if len(value) != 2:
+                #         msg = '%r in class %r improperly configured.\n!! Expecting a 2-tuple, got: %r'
+                #         raise AttributeError(msg % (name, cls_name, value))
+                #     # Add onto ordered list.
+                #     d[name] = _data(*value)
+                #     order.append(name)
+                #     continue
+                #
+                # _name = name
+                # # rename attribute: strip suffix
+                # if is_data:
+                #     name = name[:-len('__data')]
+                # if is_expected:
+                #     name = name[:-len('__expected')]
+                #
+                # # remove original attributes from dict
+                # data = d.pop(name + '__data', None)
+                # expected = d.pop(name + '__expected', None)
+                #
+                # # Guard, improperly defined attribute or second visit
+                # if not data or not expected:
+                #     # second visit
+                #     if name in order:
+                #         continue
+                #
+                #     # error handling
+                #     if data:
+                #         missing_attr = name + '__expected'
+                #     elif expected:
+                #         missing_attr = name + '__data'
+                #     else:
+                #         missing_attr = 'Unknown'
+                #
+                #     choices = [key for key in cls_dict if key != _name]
+                #     close_match = get_close_matches(missing_attr, choices, 1)
+                #     if close_match:
+                #         misspelled = '\n\nDid you mistype  %r  ?' % close_match[0]
+                #     else:
+                #         misspelled = ''
+                #
+                #     msg = '%r in class %r improperly configured.\n!! Missing attribute: %r%s'
+                #     raise AttributeError(msg % (name, cls_name, missing_attr, misspelled))
+                #
+                # # Add onto ordered list.
+                # d[name] = _data(data, expected)
+                # order.append(name)
+        for test_name in iterkeys(test_data_q):
+            for action_tag, action in iteritems(order):
+                if test_name not in action:
+                    warn(  # :off
+                        '{test_name} is missing expected results for test_{action_tag}'.format(**vars()),
+                        category=UserWarning
+                    )  # :on
 
-        d['__ordered__'] = order
+        if order:
+            d['__ordered__'] = order
         return type.__new__(cls, cls_name, bases, d)
-
-    @classmethod
-    def __prepare__(*_):
-        return OrderedDict()
 
 
 class MultiTestCaseBase(with_metaclass(MultiTestMeta)):
